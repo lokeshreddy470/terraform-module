b4a2d208164b43111eaf28adda79ab9ea3d15de9
abhisheksanga <abhisheksanga66@gmail.com>
2019-08-17 10:58:54 -0500

    Infrastuture for multi region deployments

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "test-us-east-1" {
+  source = "module"
+  region = "us-east-1"
+}
+
+module "test-us-west-1" {
+  source = "module"
+  region = "us-west-1"
+}
---
+++ module/vars.tf
@@ -0,0 +1,18 @@
+variable "region" {}
+
+variable "servers_per_az" {
+  default = 1
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+# variable "r53_zone_id" {
+#  default = "<change me>"
+# }
+
+# Will be prepended to the name associated r53_zone_id
+# variable "r53_domain_name" {
+#  default = "cdn"
+# }
